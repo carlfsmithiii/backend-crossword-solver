7ce2852b511fbbe8c99061d6e3397908b4338936
Carl Smith <carlfsmithiii@gmail.com>
2018-11-20 15:40:30 -0500

    formatting

--- xword.py
+++ xword.py
@@ -15,13 +15,13 @@ def main():
 
     test_word = raw_input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
-    
 
     # YOUR ADDITIONAL CODE HERE
     regex = test_word.replace(r' ', r'\w') + r'$'
     matches = [word for word in words if re.match(regex, word)]
     for match in matches:
         print(match)
+
 
 if __name__ == '__main__':
     main()
